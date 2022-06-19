a83c985533757f2b09f8ef02d686ae1eb0418565
Shawn Tabai <stabai@gmail.com>
2022-06-18 22:58:28 -0700

    Format tests and clean up ci steps

--- shell/commands_test.ts
+++ shell/commands_test.ts
@@ -1,17 +1,17 @@
-import { assert, assertFalse } from "https://deno.land/std@0.144.0/testing/asserts.ts";
-import { commandExists } from "commands.ts";
+import { assert, assertFalse } from 'https://deno.land/std@0.144.0/testing/asserts.ts';
+import { commandExists } from './commands.ts';
 
-Deno.test("built-in commands return true", async () => {
-  const exists = await commandExists("echo");
+Deno.test('built-in commands return true', async () => {
+  const exists = await commandExists('echo');
   assert(exists);
 });
 
-Deno.test("names of executables on path return true", async () => {
-  const exists = await commandExists("deno");
+Deno.test('names of executables on path return true', async () => {
+  const exists = await commandExists('deno');
   assert(exists);
 });
 
-Deno.test("unknown names return false", async () => {
-  const exists = await commandExists("idonotexist");
+Deno.test('unknown names return false', async () => {
+  const exists = await commandExists('idonotexist');
   assertFalse(exists);
 });
