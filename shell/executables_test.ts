352734142631b77dd8c2ec94995d00d22cf44457
Shawn Tabai <shawn.tabai@actualhq.com>
2022-07-04 23:19:28 -0700

    Fix makeFileExecutable test for windows environment

--- shell/executables_test.ts
+++ shell/executables_test.ts
@@ -2,11 +2,24 @@ import { assert, assertRejects } from 'https://deno.land/std@0.144.0/testing/ass
 import { makeFileExecutable } from './executables.ts';
 import { $ } from './run.ts';
 
-Deno.test('new file becomes executable', async () => {
+Deno.test('new file becomes executable on unix/mac', async () => {
+  if (Deno.build.os === 'windows') {
+    return;
+  }
+
   const filename = await Deno.makeTempFile();
   Deno.writeTextFile(filename, 'echo "looks good"');
   assertRejects(() => $([filename]), 'File execution succeeded before permission flag was set');
 
   await makeFileExecutable(filename);
   assert(await $([filename]), 'File execution failed after permission flag was set');
+});
+
+Deno.test('does not blow up on windows', async () => {
+  if (Deno.build.os !== 'windows') {
+    return;
+  }
+
+  const filename = await Deno.makeTempFile();
+  await makeFileExecutable(filename);
 });
