effbbbd028c0e0031e8c2bf9c330868498046079
Shawn Tabai <stabai@gmail.com>
2022-07-08 01:31:58 -0700

    Add nonExplosively helper

--- lang/errors.ts
+++ lang/errors.ts
@@ -1,5 +1,13 @@
 export type LoggingFunction = (...data: unknown[]) => void;
 
+export function nonExplosively<T>(fn: () => T): T | Error {
+  try {
+    return fn();
+  } catch (err) {
+    return err;
+  }
+}
+
 export async function catchErrorsAsWarnings(
   fn: (...args: unknown[]) => Promise<unknown>,
   logger: LoggingFunction = console.warn,
